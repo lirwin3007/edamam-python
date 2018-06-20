50e0b296079b00e7ea904381d43630378c354032
Louis Irwin <louis.irwin.3007@gmail.com>
2018-06-20 22:30:08 +0100

    Optomised package

--- Edamam Example.py
+++ Edamam Example.py
@@ -1,7 +1,7 @@
 import Edamam
 
-Edamam.appID = "818a6ce2"
-Edamam.appKey = "601ce45d5c4b47772011c677abe0211f"
+Edamam.appID = "INSERT APP ID"
+Edamam.appKey = "INSERT APP KEY"
 
 test = Edamam.RecipeSearch({'q':'scrambled egg and salmon', 'to':10}).getResults()
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="edamam-python",
+    name="edamam_api",
     version="0.0.1",
     author="Louis Irwin",
     author_email="coding@louisirwin.co.uk",
