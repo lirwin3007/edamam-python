3757606ccf613982d5e75749ec5b13166e366ee1
Louis Irwin <louis.irwin.3007@gmail.com>
2018-06-20 22:10:51 +0100

    Formatted into python package
    Formatted the project into the pypi python package format and created files necessary for upload to pypi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # edamam-python
+
+Python library to interact with the edamam online food database
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edamam-python",
+    version="0.0.1",
+    author="Louis Irwin",
+    author_email="coding@louisirwin.co.uk",
+    description="Python library to interface with the edamam API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lirwin3007/edamam-python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ),
+)
